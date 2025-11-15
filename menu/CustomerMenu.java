84179b2641349148acf1ad3d7319fb5c85f9bca0
B.ngetich <ngetichbrian14743@gmail.com>
2025-11-15 10:31:46 -0600

    Add first 2 menu function,About to design cart and continue with the rest

--- menu/CustomerMenu.java
+++ menu/CustomerMenu.java
@@ -1,24 +1,58 @@
 package menu;
 
+import java.util.Scanner;
+
+import menuFunctions.viewProducts;
+
 public class CustomerMenu {
 
-   
-    static String mainmenu = """
-            ===== STORE MENU =====
-                    1. View all products
-                    2. View products by category
-                    3. Add product to cart
-                    4. View cart
-                    5. Remove product from cart
-                    6. Apply discount (if available)
-                    7. Checkout
-                    8. Exit
-                    =======================
-                    Enter your choice: 
 
-            """;
-        public static  void showMenu(){
-                System.out.println(mainmenu);
-        }
-    
+                //import java.io.Console;
+
+                //    var  con = System.console();
+                //     if (con != null) {
+                //         Scanner sc = new Scanner(con.reader());
+                //         int i = sc.nextInt();
+                //     }
+
+                static String MainMenuOptions = """
+                        ===== STORE MENU =====
+                                1. View all products
+                                2. View products by category
+                                3. Add product to cart
+                                4. View cart
+                                5. Remove product from cart
+                                6. Apply discount (if available)
+                                7. Checkout
+                                8. Exit
+                                =======================
+                                Enter your choice: 
+
+                        """;
+                public static void showMenu() {
+                        System.out.print(MainMenuOptions);
+                
+                        Scanner sc = new Scanner(System.in);
+                       int input =  sc.nextInt();
+
+
+                       switch(input){
+                        case 1 ->{
+                                viewProducts.displayAllProducts();
+                        }
+
+                        case 2 ->{
+                                viewProducts.displayByCategory();
+                        }
+                       }
+
+
+
+                        sc.close();
+                
+                
+                }
+                
+
+
 }
